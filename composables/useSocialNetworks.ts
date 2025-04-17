8762c8e2caf18d65cb76421c2fc2fdbdf1521528
Andres <andres.antolinez@peaku.ai>
2025-04-16 19:06:47 -0500

    :zap: feat(composables): add useSearch and useSocialNetworks composables

---
+++ composables/useSocialNetworks.ts
@@ -0,0 +1,16 @@
+export const useSocialNewtworks = () => {
+	return [
+		{
+			name: 'GitHub',
+			icon: 'mdi:github',
+			url: 'https://github.com/yourprofile',
+			color: '#ffffff'
+		},
+		{
+			name: 'LinkedIn',
+			icon: 'mdi:linkedin',
+			url: 'https://linkedin.com/in/yourprofile',
+			color: '#0077B5'
+		}
+	]
+}
