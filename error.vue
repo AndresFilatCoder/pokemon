a0127b7939f0d3168b2e2563f0976ef5de5b4e66
Andres <andres.antolinez@peaku.ai>
2025-04-17 15:52:44 -0500

    :bug: feat(page): add custom error page for handling 404 and other errors

---
+++ error.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="main-content">
+		<div>
+			<img src="/public/gifs/giphy.gif" alt="Pokemon confused">
+		</div>
+		<div>
+			<h1 class="pokemon-font status-code">{{ error.statusCode }}</h1>
+			<h2 class="pokemon-font">{{ error.statusMessage }}</h2>
+			<NuxtLink to="/" class="text-reset text-decoration-none">
+				<Button class="mt-4">Volver</Button>
+			</NuxtLink>
+		</div>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	error: {
+		type: Object,
+		default: {},
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+.status-code {
+	font-size: 6rem;
+}
+.main-content {
+	display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; 
+}
+</style>
