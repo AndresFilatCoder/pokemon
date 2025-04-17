b484dd8ee88476fe29175c46edf27e20555d36c5
Andres <andres.antolinez@peaku.ai>
2025-04-16 19:19:48 -0500

    :wrench: chore(nuxt): update Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,27 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: false },
+	css: ['~/assets/css/main.scss', 'bootstrap/dist/css/bootstrap.min.css'],
 	runtimeConfig: {
 		public: {
 			apiBase: 'https://pokeapi.co/api/v2/'
 		}
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "~/assets/_colors.scss" as *;'
+				}
+			},
+			preprocessorMaxWorkers: true
+		},
+	},
+	postcss: {
+		plugins: {
+			autoprefixer: {},
+			"postcss-custom-media": {}
+		},
 	},
 	app: {
 		head: {
@@ -13,4 +30,5 @@ export default defineNuxtConfig({
 			title: 'PokePage',
 		}
 	},
+	modules: ['@nuxt/icon']
 })
