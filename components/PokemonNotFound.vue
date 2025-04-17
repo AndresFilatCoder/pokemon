3e58bc9282226c8bda4718bfe2146d9cae2f003f
Andres <andres.antolinez@peaku.ai>
2025-04-17 15:29:56 -0500

    :sparkles: feat(pokemon): add "Not Found" component for missing Pokémon

---
+++ components/PokemonNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="d-flex justify-content-center">
+		<img src="/public/img/pikachu-confused.webp" alt="Pikachu confused" width="300">
+	</div>
+	<h1 class="pokemon-font text-center mt-4">Pokemon no encontrado</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
